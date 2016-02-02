0670601fae4e00e3b60c386d3a679169ef3b3bc8
Richard B. Wagner <breckwagner@gmail.com>
2016-02-02 13:42:11 -0800

    Update + Testing

--- project_1/main_controller/main_controller.ino
+++ project_1/main_controller/main_controller.ino
@@ -1,37 +1,11 @@
-/*
- * main.cpp
- *
- *  Created on: 18-Feb-2011
- *      Author: nrqm
- *
- * Example program for time-triggered scheduling on Arduino.
- *
- * This program pulses one pin every 500 ms and another pin every 300 ms
- *
- * There are two important parts in this file:
- * 		- at the end of the setup function I call Scheduler_Init and Scheduler_StartTask.  The latter is called once for
- * 		  each task to be started (note that technically they're not really tasks because they share the stack and don't
- * 		  have separate contexts, but I call them tasks anyway because whatever).
- * 		- in the loop function, I call the scheduler dispatch function, which checks to see if a task needs to run.  If
- * 		  there is a task to run, then it its callback function defined in the StartTask function is called.  Otherwise,
- * 		  the dispatch function returns the amount of time (in ms) before the next task will need to run.  The idle task
- * 		  can then do something useful during that idle period (in this case, it just hangs).
- *
- * To use the scheduler, you just need to define some task functions and call Scheduler_StartTask in the setup routine
- * for each of them.  Keep the loop function below the same.  The scheduler will call the tasks you've created, in
- * accordance with the creation parameters.
- *
- * See scheduler.h for more documentation on the scheduler functions.  It's worth your time to look over scheduler.cpp
- * too, it's not difficult to understand what's going on here.
- */
- 
+
 #include <Arduino.h>
 #include "scheduler.h"
-#include "assets/util.h"
+#include "util.h"
 
 #include <Wire.h>
 #include <LCD.h>
-#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_I2C/LiquidCrystal_I2C.h>
 
 //I2C set up
 #define I2C_ADDR    0x3F 
@@ -71,6 +45,16 @@ int lastLaserState = laserState;
 long debounceTimer =0;
 long test = 10; 
 
+double mean(int array[]) {
+  int i = 0;
+  double avg = 0.0;
+  double len = (sizeof(array)/sizeof(int));
+  while (i++ < len) {
+    avg += array[i];
+  }
+  return avg / len;
+}
+
 
 //periodic tasks - status of laser, , and detect when pressed but miss 
 void fire_laser(){
@@ -98,10 +82,15 @@ void move_servo()
 }
 
 void laser_state(){
+  static byte i = 0;
+  static int reading[20];
+  int avg = (int)mean(reading[5]);
+
+  
   
   photoCellReading = analogRead(photoCellPin);  //take in reading from the light sensor
-   
-  if (photoCellReading > 800) {   //this value was chosen after reading the examples by AdaFruit, anything less than this is too dark to be the lazer
+  reading[i%20] = photoCellReading;
+  if (photoCellReading > avg) {   //this value was chosen after reading the examples by AdaFruit, anything less than this is too dark to be the lazer
     targetHit();                  //add status for target missed 
   }
   
@@ -187,7 +176,7 @@ void setup()
   Scheduler_StartTask(0, 10, laser_state);    //detect if target hit or missed
   Scheduler_StartTask(0, 10, move_servo);     //task for servo movement 
   Scheduler_StartTask(0, 10, missed_target); 
-  Scheduler_StartTask(0, 2, clear_bottom_row); 
+  //Scheduler_StartTask(0, 2, clear_bottom_row); 
  
 }
  
