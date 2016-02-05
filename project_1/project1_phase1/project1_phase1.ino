3fb0fb435076c3be1d1bf1cfbbb281739a7fe83b
Navdeep Bahia <nkbahia@gmail.com>
2016-02-05 00:31:11 -0800

    Cleaned up comments for Phase 1

--- project_1/project1_phase1/project1_phase1.ino
+++ project_1/project1_phase1/project1_phase1.ino
@@ -19,26 +19,38 @@ LiquidCrystal_I2C  lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin
 #define CENTER 1500
 #define THE_ANSWER_TO_THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING 42
 
+//Servo setup
 Servo s;
 int servoVal;
 int theta;
 
 // Arduino pins
-const int SW_pin = 2;  // digital pin connected to switch output
-const int X_pin = 1;  // analog pin connected to X output
-const int Y_pin = 2;  //analog pin connected to Y output
-const int photoCellPin = 0;   //analog pin connected to the light sensor
+// Digital pin connected to switch output
+const int SW_pin = 2; 
+// Analog pin connected to X output
+const int X_pin = 1;  
+//Analog pin connected to Y output
+const int Y_pin = 2; 
+//Analog pin connected to the light sensor
+const int photoCellPin = 0;   
 
+//Variable to store input from light sensor 
 int photoCellReading;  
 
-int lastButtonState = HIGH;     //start with the button reading as HIGH
+//Variable for Debouncing
+//Start with the button reading as HIGH
+int lastButtonState = HIGH;     
 int currentButtonState;
-int laserState = LOW;           //initally the laser should be OFF
-long lastDebounceTime = 0;  // the last time the button was pressed
-long debounceDelay = 50;    // the debounce time; increase if the output flickers
+//Initally the laser should be OFF
+int laserState = LOW;       
+//The last time the button was pressed   
+long lastDebounceTime = 0;  
+//The debounce time
+long debounceDelay = 50;    
 
 void setup() {
 
+  //Servo Setup
   s.attach(9);
   s.writeMicroseconds(CENTER);
   delay(15);
@@ -52,72 +64,77 @@ void setup() {
   lcd.home ();
   lcd.print("Ready for battle");  
   delay(5000);
-  
-  pinMode(SW_pin, INPUT);     //joystick button
+
+  //Joystick button
+  pinMode(SW_pin, INPUT);     
   digitalWrite(SW_pin, HIGH);
-  pinMode (13, OUTPUT);       //the laser
+  //The laser
+  pinMode (13, OUTPUT); 
+        
   Serial.begin(9600);
 
   
 }
  
 void loop() {
-  
-  digitalWrite(13, laserState);            //start with the laser off
 
-  //debouncing
-  int reading = digitalRead(SW_pin);      //read input from button
+  //Start with the laser off
+  digitalWrite(13, laserState);            
 
-   // reset the debouncing timer if the button state has changed
+  //Read input from button
+  int reading = digitalRead(SW_pin);      
+
+   //Reset the debouncing timer if the button state has changed
   if (reading != lastButtonState) {
     lastDebounceTime = micros();
   }
 
- // whatever the reading is at, it's been there for longer
- // than the debounce delay, so take it as the actual current state:
+ /* Whatever the reading is at, it's been there for longer
+  than the debounce delay, so take it as the actual current state:
+ */
   if ((micros() - lastDebounceTime) > debounceDelay) {
 
-    // if the button state has changed, change variable
+    // If the button state has changed, change variable
     if (reading != currentButtonState) {
       currentButtonState = reading;
 
-      // only toggle the laser if the new button state is LOW
+      // Only toggle the laser if the new button state is LOW
       if (currentButtonState == LOW) {
         laserState = !laserState;
       }
     }
   }
     
-  //toggle laser
+  //Toggle laser
   digitalWrite(13, laserState);
   
   //reading is saved so that it will be the lastButtonState on the next iteration
   lastButtonState = reading;
-  
-  photoCellReading = analogRead(photoCellPin);  //take in reading from the light sensor
-   
-  if (photoCellReading > 800) {   //this value was chosen after reading the examples by AdaFruit, anything less than this is too dark to be the lazer
-    targetHit();                  //add status for dark, dim, hit?
+
+  //Take in reading from the light sensor
+  photoCellReading = analogRead(photoCellPin); 
+
+  /*This value was chosen after reading the examples by AdaFruit, 
+   * anything less than this is too dark to be the laser
+   */
+  if (photoCellReading > 800) {   
+    targetHit();                 
   }
 
  
   
    servoVal = analogRead(X_pin); 
+   //Print position on X-axis
    lcd.setCursor(0,0);
    lcd.print("At position: ");
    lcd.print(servoVal);
-  //maps the joystick inputs to the range that's safe for the servo motors - note: this may need to be adjusted
-  theta = map(servoVal, 0, 1023, 1000, 2000);       
-  s.writeMicroseconds(theta);
-  delay(THE_ANSWER_TO_THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING); 
+  
+  //Maps the joystick inputs to the range that's safe for the servo motors 
+   theta = map(servoVal, 0, 1023, 1000, 2000);       
+   s.writeMicroseconds(theta);
+   delay(THE_ANSWER_TO_THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING); 
 
-  /*do we realy need to read the y-azis input? It only goes 180 degrees
-  Serial.print("\n");
-  Serial.print("Y-axis: ");  
-  Serial.println(analogRead(Y_pin));
-  Serial.print("\n\n");
-  */
-  //delay(500);
+
 }
 
 //the seonsor has detected the laser's light
