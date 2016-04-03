361aee621005bd8c8c30930a04f0eccc424fb828
Navdeep-Bahia <nkbahia@gmail.com>
2016-04-02 21:29:36 -0700

    Sync

--- project_3/src/basestation/main.c
+++ project_3/src/basestation/main.c
@@ -11,6 +11,10 @@
 
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
 
 #define LOW_BYTE(v)   ((unsigned char) (v))
 #define HIGH_BYTE(v)  ((unsigned char) (((unsigned int) (v)) >> 8))
@@ -32,7 +36,7 @@ void adc_init(void){
 //map input values to those appropriate for Roomba
 int16_t map(int16_t x, int16_t in_min, int16_t in_max, int16_t out_min, int16_t out_max){ 
   	
-	int16_t result =  (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+	int16_t result =  ((x - in_min) * (out_max - out_min)) / ((in_max - in_min) + out_min);
 	return result;
 }
 
@@ -47,21 +51,33 @@ uint16_t read_adc(uint8_t channel){
 
 
 void pole_sensors(){
-	uart0_init(UART_BAUD_SELECT(9600, F_CPU));
-
+	uart1_init(UART_BAUD_SELECT(9600, F_CPU));
+	//uart0_init(UART_BAUD_SELECT(9600, F_CPU));
+	//char str[16];
 	for(;;){
-		uint16_t x = 1023 - read_adc(14);
+		//read input from analog pins 14,15
+		uint16_t x = read_adc(14);
 		uint16_t y = read_adc(15);
-		int16_t velocity = map(y, 0, 1023, -2000, 2000);
-		int16_t radius = map(x, 0, 1023, -500, 500);
-		uart0_putc(0);
-		uart0_putc(5);
-		uart0_putc(ROOMBA_DRIVE);
-		
-		uart0_putc(HIGH_BYTE(velocity));
-		uart0_putc(LOW_BYTE(velocity));
-		uart0_putc(HIGH_BYTE(radius));
-		uart0_putc(LOW_BYTE(radius));	
+		//sprintf(str, "%d, %d\n", x,y);
+		//uart0_puts(str);
+		//init digital pin 22 and set as input
+		//DDRB &= ~_BV(DDA0);
+		//int val = PINA & _BV(PA0);
+		//map values from joystick
+		int16_t velocity = map(y, 0, 1023, -500, 500);
+		int16_t radius = 0x8000;//map(x, 0, 1023, -2000, 2000);
+		//send values to trigger manual control
+		uart1_putc(1);
+		uart1_putc(5);
+		uart1_putc(ROOMBA_DRIVE);
+		//split bytes per Roomba specs
+		uart1_putc(HIGH_BYTE(velocity));
+		uart1_putc(LOW_BYTE(velocity));
+		uart1_putc(HIGH_BYTE(radius));
+		uart1_putc(LOW_BYTE(radius));
+		//toggle laser and send input from button
+		//uart1_putc(1);
+		//uart1_putc(val);	
 		//Task_Next();
 		Task_Sleep(5);
 	}
